2116df6316797780b6814379120ec7647cec5cc3
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-05-03 17:36:02 +0530

    greatest of three numbers

--- Basicproblem1.java
+++ Basicproblem1.java
@@ -1,20 +1,46 @@
 // 1) write a java program to find out greatest of two numbers?
+// import java.util.*;
+// public class Basicproblem1 {
+//     public static void main(String[] args) {
+//         Scanner s =new Scanner(System.in);
+
+//         System.out.println("Enter the first number");
+//         int a =s.nextInt();
+
+//         System.out.println("Enter the second number");
+//         int b =s.nextInt();
+
+//         if(a>b)
+//             System.out.println(a+"is greatest number");
+//         if(a<b)
+//             System.out.println(b+"is greatest number");
+
+//         s.close();
+//     }
+// }
+
+// 2) Write a java program to find out greatest of three numbers?
+
 import java.util.*;
 public class Basicproblem1 {
     public static void main(String[] args) {
-        Scanner s =new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter the first number");
+        System.out.println("Enter the first numbers");
         int a =s.nextInt();
 
         System.out.println("Enter the second number");
         int b =s.nextInt();
 
-        if(a>b)
-            System.out.println(a+"is greatest number");
-        if(a<b)
-            System.out.println(b+"is greatest number");
+        System.out.println("Enter the third number");
+        int c =s.nextInt();
 
+        if((a>b) && (a>c))
+            System.out.println(a+"is Greatest");
+        if((b>a) &&(b>c))
+            System.out.println(b+"is Greatest");
+        if((c>a) &&(c>b))
+            System.out.println(c+"is Greatest");
         s.close();
     }
 }
