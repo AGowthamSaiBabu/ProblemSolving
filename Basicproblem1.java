1b1b1f0f2a3c7bb32736643e52b8927ade6fc829
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-05-05 17:42:33 +0530

    Basicproblem1

--- Basicproblem1.java
+++ Basicproblem1.java
@@ -21,26 +21,45 @@
 
 // 2) Write a java program to find out greatest of three numbers?
 
+// import java.util.*;
+// public class Basicproblem1 {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+
+//         System.out.println("Enter the first numbers");
+//         int a =s.nextInt();
+
+//         System.out.println("Enter the second number");
+//         int b =s.nextInt();
+
+//         System.out.println("Enter the third number");
+//         int c =s.nextInt();
+
+//         if((a>b) && (a>c))
+//             System.out.println(a+"is Greatest");
+//         if((b>a) &&(b>c))
+//             System.out.println(b+"is Greatest");
+//         if((c>a) &&(c>b))
+//             System.out.println(c+"is Greatest");
+//         s.close();
+//     }
+// }
+
+// 3 Write a java program to find out given age is eligible to vote or not?
+
 import java.util.*;
+
 public class Basicproblem1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the age");
+        int age = s.nextInt();
 
-        System.out.println("Enter the first numbers");
-        int a =s.nextInt();
-
-        System.out.println("Enter the second number");
-        int b =s.nextInt();
-
-        System.out.println("Enter the third number");
-        int c =s.nextInt();
-
-        if((a>b) && (a>c))
-            System.out.println(a+"is Greatest");
-        if((b>a) &&(b>c))
-            System.out.println(b+"is Greatest");
-        if((c>a) &&(c>b))
-            System.out.println(c+"is Greatest");
+        if (age >= 18) {
+            System.out.println("eligible to vote");
+        } else {
+            System.out.println("u not eligible to vote");
+        }
         s.close();
     }
 }
