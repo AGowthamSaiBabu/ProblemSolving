1eb4a2a47a0f04910bea01215b8e1d1b74a9fd15
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-05-27 16:13:11 +0530

    Basicproblem2

--- Basicproblem2.java
+++ Basicproblem2.java
@@ -48,27 +48,57 @@
 
 // 13) Write a java program to accept six marks of a student then find out total, average and grade?
 
+// import java.util.*;
+
+// public class Basicproblem2 {
+//     public static void main(String[] args) {
+//         int m1=89,m2=36,m3=44,m4=51,m5=62,m6=73,total=0;
+        
+
+// 		total=m1+m2+m3+m4+m5+m6;
+// 		float average = (float)total/6;
+
+// 		System.out.println("Total :"+total);
+// 		System.out.printf("Average : %.2f \n",average);
+
+// 		if(average>=70  && average<=100)
+// 			System.out.println("Grade : A grade");
+// 		else if(average >=50 && average<=69)
+// 			System.out.println("Grade : B grade");
+// 		else if(average >=35 && average <=49)
+// 			System.out.println("Grade : C grade");
+// 		else 
+// 			System.out.println("Grade : Failed");
+
+//     }
+// }
+
+
+// 14) Write a java program to accept week number (1-7) then display the day of the week?
+
 import java.util.*;
 
 public class Basicproblem2 {
     public static void main(String[] args) {
-        int m1=89,m2=36,m3=44,m4=51,m5=62,m6=73,total=0;
-        
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the Week days");
+	int week =s.nextInt();
 
-		total=m1+m2+m3+m4+m5+m6;
-		float average = (float)total/6;
-
-		System.out.println("Total :"+total);
-		System.out.printf("Average : %.2f \n",average);
-
-		if(average>=70  && average<=100)
-			System.out.println("Grade : A grade");
-		else if(average >=50 && average<=69)
-			System.out.println("Grade : B grade");
-		else if(average >=35 && average <=49)
-			System.out.println("Grade : C grade");
-		else 
-			System.out.println("Grade : Failed");
-
+	if(week==1)
+		System.out.println("Monday");
+	else if(week==2)
+		System.out.println("Tuesaday");
+	else if(week==3)
+		System.out.println("Wednesday");
+	else if(week==4)
+		System.out.println("Thursday");
+	else if(week==5)
+		System.out.println("Friday");
+	else if(week==6)
+		System.out.println("Saturday");
+	else if(week==7)
+		System.out.println("Sunday");
+	else
+		System.out.println("Invalid week number");
     }
 }
