2e367d97233d8a8cab8c1e3e68460ed26f2a48ff
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-07-22 11:00:22 +0530

    Basicproblem3

--- Basicproblem3.java
+++ Basicproblem3.java
@@ -1,16 +1,44 @@
+// 21) Write a java program to find out given number is positive or negative by using nested if stmt?
+
+// import java.util.*;
+// public class Basicproblem3{
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter the Number");
+//         int number = s.nextInt();
+
+//         if(number!=0){
+//             if(number>0){
+//                 System.out.println("It is a positive number");
+//                 System.exit(0);
+//             }
+//             System.out.println("It is a Negative number");
+//         }
+//     }
+// }
+
+//22) Month names using switch case
+
 import java.util.*;
 public class Basicproblem3{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the Number");
-        int number = s.nextInt();
+        System.out.println("Enter The Number Days");
+        String days =s.nextLine();
 
-        if(number!=0){
-            if(number>0){
-                System.out.println("It is a positive number");
-                System.exit(0);
-            }
-            System.out.println("It is a Negative number");
+        switch(days){
+            case "one":
+                System.out.println("Jan");
+                break;
+            case "two":
+                System.out.println("Feb"); 
+                break;
+            case "three":
+                System.out.println("Mar");
+                break;  
+            default:
+                System.out.println("coming soon");         
         }
+
     }
 }
