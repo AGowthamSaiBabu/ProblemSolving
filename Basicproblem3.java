3e9becc83759176d8d6e02833a6be74ef99988a1
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-08-04 09:20:56 +0530

    Basicproblem3

--- Basicproblem3.java
+++ Basicproblem3.java
@@ -19,26 +19,40 @@
 
 //22) Month names using switch case
 
+// import java.util.*;
+// public class Basicproblem3{
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter The Number Days");
+//         String days =s.nextLine();
+
+//         switch(days){
+//             case "one":
+//                 System.out.println("Jan");
+//                 break;
+//             case "two":
+//                 System.out.println("Feb"); 
+//                 break;
+//             case "three":
+//                 System.out.println("Mar");
+//                 break;  
+//             default:
+//                 System.out.println("coming soon");         
+//         }
+//         s.close();
+//     }
+// }
+
+//23) Write a java program to display 10 natural numbers?
+
 import java.util.*;
 public class Basicproblem3{
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter The Number Days");
-        String days =s.nextLine();
+        int i=1;
 
-        switch(days){
-            case "one":
-                System.out.println("Jan");
-                break;
-            case "two":
-                System.out.println("Feb"); 
-                break;
-            case "three":
-                System.out.println("Mar");
-                break;  
-            default:
-                System.out.println("coming soon");         
+        while(i<=10){
+            System.out.println(i);
+            i++;
         }
-
     }
 }
